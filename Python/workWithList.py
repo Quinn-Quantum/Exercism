0697e76b72c42e535a1adee3b68bcc06dbd214cc
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2024-02-15 18:47:38 +0100

    add: task

---
+++ Python/workWithList.py
@@ -0,0 +1,42 @@
+
+def remove_the_mean_person(queue, person_name):
+    """Remove the mean person from the queue by the provided name.
+
+    :param queue: list - names in the queue.
+    :param person_name: str - name of mean person.
+    :return: list - queue update with the mean persons name removed.
+    """ 
+    queue.remove(person_name)
+    return queue
+
+
+def how_many_namefellows(queue, person_name):
+    """Count how many times the provided name appears in the queue.
+
+    :param queue: list - names in the queue.
+    :param person_name: str - name you wish to count or track.
+    :return: int - the number of times the name appears in the queue.
+    """
+
+    return queue.count(person_name)
+
+
+def remove_the_last_person(queue):
+    """Remove the person in the last index from the queue and return their name.
+
+    :param queue: list - names in the queue.
+    :return: str - name that has been removed from the end of the queue.
+    """
+    name = queue[-1]
+    queue.pop(-1)
+    return name
+
+
+def sorted_names(queue):
+    """Sort the names in the queue in alphabetical order and return the result.
+
+    :param queue: list - names in the queue.
+    :return: list - copy of the queue in alphabetical order.
+    """
+
+    return sorted(queue)
